9f2229b0ce25540a0394d80710423847d8a59127
EC2 Default User <ec2-user@ip-172-31-26-196.ap-northeast-1.compute.internal>
2021-04-14 18:32:17 +0900

    make shopping cart2

--- routes/web.php
+++ routes/web.php
@@ -19,11 +19,14 @@ Route::get('users/carts', 'CartController@index')->name('carts.index');
 
 Route::post('users/carts', 'CartController@store')->name('carts.store');
 
+Route::put('users/carts', 'CartController@update')->name('carts.update');
+
 Route::delete('users/carts', 'CartController@destroy')->name('carts.destroy');
 
 Route::get('users/mypage', 'UserController@mypage')->name('mypage');
 Route::get('users/mypage/edit', 'UserController@edit')->name('mypage.edit');
 Route::put('users/mypage', 'UserController@update')->name('mypage.update');
+Route::get('users/mypage/favorite', 'UserController@favorite')->name('mypage.favorite');
 Route::get('users/mypage/password/edit', 'UserController@edit_password')->name('mypage.edit_password');
 Route::put('users/mypage/password', 'UserController@update_password')->name('mypage.update_password');
 
